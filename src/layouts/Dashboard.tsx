3d8e40affd70502ece92c2ab1d38888e24ab8395
Deepak Barwal <deepakbarwal98@gmail.com>
2025-01-04 18:53:24 +0530

    add: protected routes

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { useAuthStore } from "../store";
 
 export default function Dashboard() {
+  const { user } = useAuthStore();
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />;
+  }
+
   return (
     <div>
       <Outlet />
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { useAuthStore } from "../store";
 
 export default function NonAuth() {
+  const { user } = useAuthStore();
+  if (user !== null) {
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div>
       <Outlet />
